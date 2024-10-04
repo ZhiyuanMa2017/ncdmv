271145b5d338da8ddd64841c9e880d0081bfad86
Zhiyuan Ma <631253076@qq.com>
2024-10-04 00:58:26 -0400

    update shell script

--- ncdmv.sh
+++ ncdmv.sh
@@ -2,5 +2,10 @@
 
 export PATH=$PATH:/usr/local/go/bin
 
+locations="cary clayton durham-east durham-south fuquay-varina garner raleigh-east raleigh-north raleigh-west wendell"
+
 cd /home/ubuntu/ncdmv
-go run ./cmd/server -appt_type=driver-license-renewal -database_path=./database/ncdmv.db -locations=cary,clayton,durham-east,durham-south,fuquay-varina,garner,raleigh-east,raleigh-north,raleigh-west,wendell -interval=15 --discord_webhook=WEBHOOK >> ./logfile.log 2>&1
+
+for location in $locations; do
+    go run ./cmd/server -appt_type=driver-license-renewal -database_path=./database/ncdmv.db -locations=$location --discord_webhook=WEBHOOk >> ./logfile.log 2>&1
+done
